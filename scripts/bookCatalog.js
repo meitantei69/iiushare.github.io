a5ead4c215045a7e573d97f52a8dfeec740563d5
KhairinN <76657940+KhairinN@users.noreply.github.com>
2020-12-30 01:27:20 +0800

    Add files via upload

---
+++ scripts/bookCatalog.js
@@ -0,0 +1,21 @@
+function modeChange() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+tbutton = document.getElementById("top");
+
+window.onscroll = function() {scrollWindow()};
+
+function scrollWindow() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    tbutton.style.display = "block";
+  } else {
+    tbutton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
