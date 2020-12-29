c3e05316533cad8a7095cb0a4b8ecc2fdb1800ed
meitantei69 <66460248+meitantei69@users.noreply.github.com>
2020-12-29 20:53:35 +0800

    Update feedback.js

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -20,8 +20,9 @@ function validate() {
   if (window.confirm('Click "OK" to continue. ')) {
     alert("Feedback submitted. ")
     window.location = "thank_you.html"
+    return true;
   }
   else {
-    return;
+    return false;
   }
 }
