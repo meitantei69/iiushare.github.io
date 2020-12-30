9053b20be73c967a911c7ec787e37df0716a1c0d
AriefYolo <76680376+AriefYolo@users.noreply.github.com>
2020-12-30 20:10:57 +0800

    Create bookCatalog.js

---
+++ scripts/bookCatalog.js
@@ -0,0 +1,79 @@
+function seeMore1()
+{
+  var dots1= document.getElementById("dots1");
+  var moreText1= document.getElementById("more1");
+  var btnText1= document.getElementById("myBtn1");
+  
+  if(dots1.style.display === "none")
+  {
+    dots1.style.display="inline";
+    btnText1.innerHTML="Read More";
+    moreText1.style.display="none";
+  }
+  else 
+  {
+    dots1.style.display="none";
+    btnText1.innerHTML="Read Less";
+    moreText1.style.display="inline";
+  }
+}
+
+function seeMore2()
+{
+  var dots2= document.getElementById("dots2");
+  var moreText2= document.getElementById("more2");
+  var btnText2= document.getElementById("myBtn2");
+  
+  if(dots2.style.display === "none")
+  {
+    dots2.style.display="inline";
+    btnText2.innerHTML="Read More";
+    moreText2.style.display="none";
+  }
+  else 
+  {
+    dots2.style.display="none";
+    btnText2.innerHTML="Read Less";
+    moreText2.style.display="inline";
+  }
+}
+
+function seeMore3()
+{
+  var dots3= document.getElementById("dots3");
+  var moreText3= document.getElementById("more3");
+  var btnText3= document.getElementById("myBtn3");
+  
+  if(dots3.style.display === "none")
+  {
+    dots3.style.display="inline";
+    btnText3.innerHTML="Read More";
+    moreText3.style.display="none";
+  }
+  else 
+  {
+    dots3.style.display="none";
+    btnText3.innerHTML="Read Less";
+    moreText3.style.display="inline";
+  }
+}
+
+function seeMore4()
+{
+  var dots4= document.getElementById("dots4");
+  var moreText4= document.getElementById("more4");
+  var btnText4= document.getElementById("myBtn4");
+  
+  if(dots4.style.display === "none")
+  {
+    dots4.style.display="inline";
+    btnText4.innerHTML="Read More";
+    moreText4.style.display="none";
+  }
+  else 
+  {
+    dots4.style.display="none";
+    btnText4.innerHTML="Read Less";
+    moreText4.style.display="inline";
+  }
+}
